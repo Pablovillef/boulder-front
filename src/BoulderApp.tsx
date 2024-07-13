cb8d15f9d9b227a4b36110020a12dd87d4779b66
Pablo <pablovillef@gmail.com>
2024-07-13 10:17:56 +0200

    Configuracion basica de Stack Navigator

--- src/BoulderApp.tsx
+++ src/BoulderApp.tsx
@@ -3,20 +3,12 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-// import Home from './presentation/screens/home/Home';
-import Login from './presentation/screens/login/Login';
-
-
-const Stack = createStackNavigator();
+import { Navigator } from './presentation/navigator/Navigator';
 
 export const BoulderApp: React.FC = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-        {/* <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} /> */}
-      </Stack.Navigator>
+      <Navigator />
     </NavigationContainer>
   );
 }
--- src/presentation/navigator/Navigator.tsx
+++ src/presentation/navigator/Navigator.tsx
@@ -1,16 +1,19 @@
 /* eslint-disable prettier/prettier */
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../screens/home/Home';
+import Login from '../screens/login/Login';
 
 const Stack = createStackNavigator();
 
 export const Navigator = () => {
   return (
-    <Stack.Navigator screenOptions={{
-      headerShown: false,
-    }}>
 
-      <Stack.Screen name="Home"component={Home} />
-    </Stack.Navigator>
+      <Stack.Navigator screenOptions={{
+        headerShown: false,
+      }}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+
   );
 };
