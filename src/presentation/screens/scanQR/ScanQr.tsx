57b38be0cae62343b0bc9c21900ce88ed07ff51b
Pablo <pablovillef@gmail.com>
2024-07-25 14:07:48 +0200

    Creada alerta que muestra el texto del codigo QR en su lectura.

--- src/presentation/screens/scanQR/ScanQr.tsx
+++ src/presentation/screens/scanQR/ScanQr.tsx
@@ -1,14 +1,18 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { RNCamera } from 'react-native-camera';
+import { Alert } from 'react-native';
+//import { RNCamera } from 'react-native-camera';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 
 const ScanQr = () => {
   return (
     <QRCodeScanner
-        onRead={({}) => console.log('Lectura QR completada')}
-        flashMode={RNCamera.Constants.FlashMode.torch}
-      />
+        onRead={({data}) => Alert.alert(data)}
+        //flashMode={RNCamera.Constants.FlashMode.torch}
+        showMarker={true}
+        reactivate={true}
+        reactivateTimeout={3000} //ms
+    />
   );
 };
 
