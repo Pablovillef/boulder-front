8389bcfee5eb8b50cfdef2367342de1fbab80237
Pablo <pablovillef@gmail.com>
2024-08-03 18:52:00 +0200

    Actualizando el componente ScanQr, para realizar peticion http con los datos obtenidos del QR.

--- src/presentation/screens/scanQR/ScanQr.tsx
+++ src/presentation/screens/scanQR/ScanQr.tsx
@@ -4,13 +4,32 @@ import React from 'react';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import { useNavigation } from '@react-navigation/native';
 import { ScanQrScreenNavigationProp } from '../../interfaces/types';
+import axios from 'axios';
 
 const ScanQr = () => {
 
   const navigation = useNavigation<ScanQrScreenNavigationProp>();
 
-  const handleQrReaded = () => {
-    navigation.navigate('DetallesVia');
+  const handleQrReaded = async (e: any) => {
+
+    // 1º Recuperar String del QR.
+    const qrData = e.data;
+    const [name, id] = qrData.split('/');
+
+    // 2º Peticion http get, con los datos del QR.
+    try{
+
+      const response = await axios.get(`http://localhost:8080/api/v1/boulder/${name}/route/${id}`);
+      const viaData = response.data;
+
+      // 3º Ir al componente DetalleVia, pasandole los datos obtenidos de la petición como parámetros.
+      navigation.navigate('DetallesVia', {viaData});
+
+    }catch(error){
+      console.error('Error obteniendo los datos de la via:', error);
+    }
+
+
   };
 
   return (
