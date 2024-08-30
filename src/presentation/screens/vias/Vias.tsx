74c71f344c3437b44bc4e5b4e21b94794e4b279b
Pablo <pablovillef@gmail.com>
2024-08-30 16:12:07 +0200

    Creada logica para poder editar una ruta, desde la vista de Vias.

--- src/presentation/screens/vias/Vias.tsx
+++ src/presentation/screens/vias/Vias.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import { View, Text, FlatList, StyleSheet, TouchableOpacity  } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, TextInput, Modal  } from 'react-native';
 import { useRoute, useNavigation  } from '@react-navigation/native';
 import { RootStackParamList, Route, ViasScreenRouteProp } from '../../interfaces/types';
 import { StackNavigationProp } from '@react-navigation/stack';
@@ -13,6 +13,51 @@ const Vias: React.FC = () => {
   const route = useRoute<ViasScreenRouteProp>();
   const {boulder, routesData, user} = route.params;
   const navigation = useNavigation<NavigationProp>();
+
+
+
+  const [routeList, setRouteList] = useState<Route[]>(routesData); // Estado para la lista de videos
+
+  const [editingRoute, setEditingRoute] = useState<Route | null>(null); // Video en edición
+  const [editModalVisible, setEditModalVisible] = useState<boolean>(false); // Estado para el modal
+  const [editQrRoute, setEditQrRoute] = useState<string>('');
+  const [editName, setEditName] = useState<string>('');
+  const [editTypeRoute, setEditTypeRoute] = useState<string>('');
+  const [editNum_nivel, setEditNum_nivel] = useState<number>(0);
+  const [editPresa, setEditPresa] = useState<string>('');
+
+  const handleEdit = (item: Route) => {
+    setEditingRoute(item);
+    setEditQrRoute(item.qrRoute);
+    setEditName(item.name);
+    setEditTypeRoute(item.typeRoute);
+    setEditNum_nivel(item.num_nivel);
+    setEditPresa(item.presa);
+    setEditModalVisible(true);
+  };
+
+  const handleSaveEdit = async () => {
+    if (editingRoute) {
+      try {
+        const updatedRoute = {
+          qrRoute: editQrRoute,
+          name: editName,
+          typeRoute: editTypeRoute,
+          num_nivel: editNum_nivel,
+          presa: editPresa,
+        };
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const response = await axios.put(`${API_BASE_URL_LOCAL}/route/${editingRoute.idRoute}`, updatedRoute);
+        const updatedRouteList = routeList.map(route =>
+          route.idRoute === editingRoute.idRoute ? { ...route, ...updatedRoute } : route
+        );
+        setRouteList(updatedRouteList);
+        setEditModalVisible(false);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
 
   console.log('Boulder:', boulder);
   console.log('RoutesData:', routesData);
@@ -62,7 +107,7 @@ const Vias: React.FC = () => {
       </View>
       <View>
         <FlatList
-          data={routesData}
+          data={routeList}
           keyExtractor={item => item.idRoute.toString()}
           renderItem={({item}) => (
           <>
@@ -76,7 +121,7 @@ const Vias: React.FC = () => {
                 </TouchableOpacity>
                 {isWorker && (
                 <>
-                <TouchableOpacity style={styles.editButton}>
+                <TouchableOpacity style={styles.editButton} onPress={() => handleEdit(item)}>
                   <Text style={styles.editButtonText}>✏️</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.editButton}>
@@ -92,6 +137,68 @@ const Vias: React.FC = () => {
       </View>
     </View>
 
+    <Modal
+        animationType= "none"
+        transparent={true}
+        visible={editModalVisible}
+        onRequestClose={() => setEditModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Editar Ruta</Text>
+
+            <Text style={styles.label}>QR de la ruta</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="QR"
+              value={editQrRoute}
+              onChangeText={setEditQrRoute}
+            />
+
+            <Text style={styles.label}>Nombre de la ruta</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Nombre"
+              value={editName}
+              onChangeText={setEditName}
+            />
+
+            <Text style={styles.label}>Tipo de ruta</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Tipo"
+              value={editTypeRoute}
+              onChangeText={setEditTypeRoute}
+            />
+
+            <Text style={styles.label}>Nivel de la ruta</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Nivel"
+              keyboardType="numeric"
+              value={editNum_nivel.toString()}
+              onChangeText={(text) => setEditNum_nivel(Number(text))}
+            />
+
+            <Text style={styles.label}>Color de las presas</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Duración"
+              value={editPresa}
+              onChangeText={setEditPresa}
+            />
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity style={styles.saveButton} onPress={handleSaveEdit}>
+                <Text style={styles.buttonText}>Guardar</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.cancelButtonModal} onPress={() => setEditModalVisible(false)}>
+                <Text style={styles.buttonText}>Cancelar</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
+
 
     <TouchableOpacity style={styles.cancelButton} onPress={() => handleBackButton()}>
       <Text style={styles.cancelButtonText}>VOLVER</Text>
@@ -101,6 +208,51 @@ const Vias: React.FC = () => {
 };
 
 const styles = StyleSheet.create({
+  cancelButtonModal: {
+    padding: 10,
+    backgroundColor: '#f44336', // Color para el botón de cancelar
+    borderRadius: 5,
+    flex: 1,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20, // Separación entre los campos y los botones
+  },
+  saveButton: {
+    padding: 10,
+    backgroundColor: '#4CAF50', // Color para el botón de guardar
+    borderRadius: 5,
+    flex: 1,
+    marginRight: 10, // Separación entre los dos botones
+  },
+  input: {
+    borderBottomWidth: 1,
+    marginBottom: 20,
+    padding: 8,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+    color: '#333',
+},
+modalContainer: {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.5)',
+},
+modalContent: {
+  width: 300,
+  padding: 20,
+  backgroundColor: '#fff',
+  borderRadius: 10,
+},
+modalTitle: {
+  fontSize: 25,
+  fontWeight: 'bold',
+  marginBottom: 30,
+  color: '#000000',
+},
   editButton: {
     marginRight: 2,
     padding: 10,
