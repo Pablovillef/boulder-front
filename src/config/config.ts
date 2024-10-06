7567a870e02e02f1c2bb9b6e486707e6428fd51a
Pablovillef <pablovillef@gmail.com>
2024-10-06 20:31:11 +0200

    Eliminada propiedad Duration de los videos. (#29)

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 // config.ts
 
-export const API_BASE_URL_LOCAL = 'http://192.168.243.215:8080/api/v1';
+export const API_BASE_URL_LOCAL = 'http://192.168.47.215:8080/api/v1';
 
 export const API_BASE_URL_PRO = 'https://climbapp.onrender.com/api/v1';
