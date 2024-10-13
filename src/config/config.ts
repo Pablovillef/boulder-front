dae313bba9723fd1fc2fdb46fe205afff4fd525b
Pablovillef <pablovillef@gmail.com>
2024-10-13 11:28:19 +0200

    Fix/formulario video (#35)
    * Corregido estilo formulario creacion de usuario.
    
    * Corregido formulario de creacion de video, para contemplar todos los posibles escenarios.

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,5 @@
 /* eslint-disable prettier/prettier */
-// config.ts
 
-export const API_BASE_URL_LOCAL = 'http://192.168.95.215:8080/api/v1';
+export const API_BASE_URL_LOCAL = 'http://192.168.160.215:8080/api/v1';
 
 export const API_BASE_URL_PRO = 'https://climbapp.onrender.com/api/v1';
